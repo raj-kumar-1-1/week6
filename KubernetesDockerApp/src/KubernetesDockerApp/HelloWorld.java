f2307706dfed5121f4dbc7c95c5971509dd7a8d1
rajkumar8853 <22h51a05e8@cmrcet.ac.in>
2024-12-08 18:05:04 +0530

    initial commit

---
+++ KubernetesDockerApp/src/KubernetesDockerApp/HelloWorld.java
@@ -0,0 +1,8 @@
+package KubernetesDockerApp;
+
+public class HelloWorld {
+	 public static void main(String[] args) {
+	        System.out.println("Hello, Kubernetes and Docker!");
+	    }
+
+}
---
+++ KubernetesDockerApp/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module KubernetesDockerApp {
+}
